aff369fdfcac00c7d3ed25fd2f648db6304151c4
Chanchal Kumar Mishra <chanchalmishra1947@gmail.com>
2021-06-17 21:33:45 +0530

    added contact

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 var navbar = document.getElementById("nav");
 
@@ -10,4 +10,14 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
-} 
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  let fname = document.getElementsByName("fname")[0].value;
+  let lname = document.getElementsByName("lname")[0].value;
+  let message = document.getElementsByName("message")[0].value;
+  let sendmail = document.getElementById("sendmail");
+  sendmail.href = `mailto:19bcs1090@gmail.com?subject=Message%20From%20Portfolio&body=Hello%20Chanchal, %0D%0A %0D%0A ${message} %0D%0A %0D%0A Thanks, %0D%0A ${fname}%20${lname}`;
+  sendmail.click();
+}
